3d604fb120204db6f8a4df9a96dfe49c8bbe09c6
todorovi <todorovi@aforbes.co.za>
2020-02-10 16:19:00 +0200

    Add project files.

---
+++ IdentityServer4Workshop/IdentityServerConfig.cs
@@ -0,0 +1,60 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace IdentityServer4Workshop
+{
+    public static class IdentityServerConfig
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis(IConfiguration configuration)
+        {
+            return new List<ApiResource>
+            {
+            };
+        }
+
+        public static IEnumerable<Client> GetClients(IConfiguration configuration)
+        {
+            return new List<Client> {
+                new Client()
+                {
+                    ClientId = "Mobile",
+                    ClientName = "Mobile service",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets ={
+                        new Secret("fU7fRb+g6YdlniuSqviOLWNkda1M/MuPtH6zNI9inF8=".Sha256())
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser> {
+                new TestUser()
+                {
+                    SubjectId = "test",
+                    Username = "test",
+                    Password = "test",
+                    IsActive = true
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServer4Workshop/ResourceOwnerPasswordValidator.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.Validation;
+using System.Threading.Tasks;
+using static IdentityModel.OidcConstants;
+
+namespace IdentityServer4Workshop
+{
+    /// <summary>
+    /// Custom Identity Server password validator
+    /// </summary>
+    public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        /// <summary>
+        /// Authenticate the user
+        /// </summary>
+        /// <param name="context">Describes the user details</param>
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            context.Result = new GrantValidationResult(context.UserName, AuthenticationMethods.Password);
+        }
+    }
+}
